9b11d7a76dfc7f4357f8c2da6d2296812058df25
Hugo <h.brunet13@gmail.com>
2018-12-02 16:17:32 +0100

    fetchOrders(all, opened, closed), fetchOrder, deleteOrder

--- main.js
+++ main.js
@@ -12,14 +12,24 @@ testCezex = async () =>{
     });
 
     console.log(cezex.name,'\n');
-    //console.log("market:\n",await cezex.fetchMarkets());
+    //console.log("CEZEX market=>\n",await cezex.fetchMarkets());
     //console.log("\ntickers:\n",await cezex.fetchTickers());
     //console.log('cezex: BTC/USD =>\n', await cezex.fetchTicker('BTC/USD')); 
     //console.log('\nCEZEX orderbook BTC/USD => \n',await cezex.fetchOrderBook("BTC/USD", 5, 5))
     //console.log('\nCEZEX trades BTC/USD => \n',await cezex.fetchTrades("BTC/USD",10))
     //console.log('\nCEZEX OHLCV BTC/USD => \n',await cezex.fetchOHLCV("BTC/USD",'1d'))
-    console.log('CEZEX fetchOrder(\'BTC/USD\') => \n', await cezex.fetchOrders('BTC/USD'));
+
+    console.log('CEZEX fetchOpenOrders(\'BTC/USD\') OPEN order  => \n', await cezex.fetchOpenOrders('BTC/USD'));
+    console.log('\n-----------------\nCEZEX fetchOrders(\'BTC/USD\') CLOSED orders => \n', await cezex.fetchClosedOrders('BTC/USD'));
+    console.log('\n-----------------\nCEZEX fetchOrders(\'BTC/USD\') ALL orders => \n', await cezex.fetchOrders('BTC/USD'));
+
+    //console.log('CEZEX FetchOrder(ID=1191) => \n', await cezex.fetchOrder(1191,'DASH/ETH'));
+    //console.log('CEZEX createOrder(\'BTC/USD\') => \n', await cezex.createOrder('BTC/USD','limit','sell',1,11500));
+
+
+   // console.log('CEZEX delete(ID=1191) => \n', await cezex.cancelOrder(1191,'DASH/ETH'));
 }
+
 
 testBxinth = async () =>{
     let bxinth    = new ccxt.bxinth();
@@ -32,25 +42,25 @@ testBxinth = async () =>{
 testBinance = async () =>{
     let binance    = new ccxt.binance();
     console.log(binance.name);
-    //console.log("market:\n",await binance.fetchMarkets());
+    console.log("BINANCE markets=>\n",await binance.fetchMarkets());
     //console.log("\ntickers:\n",await binance.fetchTickers());
     //console.log('binanc => BTC/USD =>\n', await binance.fetchTicker("BTC/USD"))
 
     //console.log('\nBINANCE orderbook BTC/USD => \n',await binance.fetchOrderBook("BTC/USDT",5))
     //console.log('\nBINANCE trades BTC/USD => \n',await binance.fetchTrades("BTC/USDT"))
     //console.log('\nBINANCE OHLCV BTC/USD => \n',await binance.fetchOHLCV("BTC/USDT",'3m'))
-    console.log('\nBINANCE orders => \n', await binance.fetchOrders("BTC/USDT"))
+  //  console.log('\nBINANCE orders => \n', await binance.fetchOrders("BTC/USDT"))
 }   
 
 testBitfinex= async () =>{
     let bitfinex    = new ccxt.bitfinex();
     console.log(bitfinex.name);
- //   console.log("market:\n",await bitfinex.fetchMarkets());
+   console.log("market:\n",await bitfinex.fetchMarkets());
     //console.log("\ntickers:\n",await bitfinex.fetchTickers());
    // console.log('bitfinex: BTC/USD =>\n', await coinex.fetchTicker("BTC/USD"))
   // console.log('\nBITFINEX orderbook BTC/USD => \n',await bitfinex.fetchOrderBook("BTC/USDT",5))
   //console.log('\n BITFINEX trades BTC/USD => \n',await bitfinex.fetchTrades("BTC/USDT")) 
-  console.log('\nBITFINEX OHLCV BTC/USD => \n',await bitfinex.fetchOHLCV("BTC/USDT"))
+  //console.log('\nBITFINEX OHLCV BTC/USD => \n',await bitfinex.fetchOHLCV("BTC/USDT"))
   await bitfinex.Dept
 }
 
